64a8ec64036c0ffb723e3f7a6ca8705aba2c7371
User <User@LAB3-7>
2021-11-03 14:35:16 +0200

    added context 4 views

--- ZofimPortalApp/ZofimPortalApp/Views/EditActivities.xaml.cs
+++ ZofimPortalApp/ZofimPortalApp/Views/EditActivities.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZofimPortalApp.ViewModels;
 
 namespace ZofimPortalApp.Views
 {
@@ -14,6 +15,8 @@ namespace ZofimPortalApp.Views
     {
         public EditActivities()
         {
+            EditActivitiesVM vm = new EditActivitiesVM();
+            this.BindingContext = vm;
             InitializeComponent();
         }
     }
