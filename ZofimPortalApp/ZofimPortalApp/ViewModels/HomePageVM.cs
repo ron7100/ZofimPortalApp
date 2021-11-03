7cb9259b033fe461a0bf0ed5eb026e86f4b8c7c7
User <User@LAB3-7>
2021-11-03 14:31:09 +0200

    Added model & view models

---
+++ ZofimPortalApp/ZofimPortalApp/ViewModels/HomePageVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using ZofimPortalApp.Services;
+using ZofimPortalApp.Models;
+using Xamarin.Essentials;
+using System.Linq;
+
+namespace ZofimPortalApp.ViewModels
+{
+    class HomePageVM
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ ZofimPortalApp/ZofimPortalApp/Views/ManageShevet.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ZofimPortalApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ManageShevet : ContentPage
+    {
+        public ManageShevet()
+        {
+            InitializeComponent();
+        }
+    }
+}
